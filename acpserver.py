385722dd6f8ea9e8efa0ffcd80d99b0a3a477d58
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-03-14 11:30:49 +1100

    change to run_agent_provider

--- acpserver.py
+++ acpserver.py
@@ -4,14 +4,17 @@ from acp.server.highlevel import Server, Context
 from beeai_sdk.providers.agent import run_agent_provider
 from beeai_sdk.schemas.text import TextOutput, TextInput
 
-acp = Server("randoagentserver")
 
+def run():
+    server = Server("randoagentserver")
 
-@server.agent()
-async def run_crewai_agent(input: TextInput, ctx: Context) -> TextOutput:
-    # Core logic for the "hello-world" agent
-    return TextOutput(text=f"Hi there {input.text}")
+    @server.agent()
+    async def run_crewai_agent(input: TextInput, ctx: Context) -> TextOutput:
+        # Core logic for the "hello-world" agent
+        return TextOutput(text=f"Hi there {input.text}")
+
+    run_agent_provider(server)
 
 
 if __name__ == "__main__":
-    asyncio.run(run_crewai_agent(server))
+    asyncio.run(run())
