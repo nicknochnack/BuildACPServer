b0596d6032cba0930b22648d96bb53e8cf3b3d5d
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-04-08 10:28:55 +1000

    convert to dockerized version

--- src/acpserver/server.py
+++ src/acpserver/server.py
@@ -1,38 +1,22 @@
-import asyncio
-
-from acp.server.highlevel import Server, Context
-from beeai_sdk.providers.agent import run_agent_provider
+from beeai_sdk.providers.agent import Server
 from beeai_sdk.schemas.text import TextOutput, TextInput
 from smolagents import CodeAgent, DuckDuckGoSearchTool, LiteLLMModel, VisitWebpageTool
 
-def main():
-    server = Server("randoagentserver")
+server = Server("randoagentserver")
 
-    @server.agent(
-        "hello-world",
-        "this is my hello world agent",
-        input=TextInput,
-        output=TextOutput,
+@server.agent()
+async def run_crewai_agent(input: TextInput, ctx: Context) -> TextOutput:
+    
+    model = LiteLLMModel(
+            model_id="ollama_chat/qwen2.5:14b",
+            api_base="http://localhost:11434",  
+            api_key="your-api-key",
+            num_ctx=8192) 
+
+    agent = CodeAgent(tools=[DuckDuckGoSearchTool(), VisitWebpageTool()], model=model)
+
+    response = agent.run(
+        input.text
     )
-    async def run_crewai_agent(input: TextInput, ctx: Context) -> TextOutput:
-        
-        model = LiteLLMModel(
-                # model_id="ollama_chat/llama3.1:8b-beeai",
-                model_id="ollama_chat/qwen2.5:14b",
-                api_base="http://localhost:11434",  # replace with remote open-ai compatible server if necessary
-                api_key="your-api-key",  # replace with API key if necessary
-                num_ctx=8192) 
 
-        agent = CodeAgent(tools=[DuckDuckGoSearchTool(), VisitWebpageTool()], model=model)
-
-        response = agent.run(
-            input.text
-        )
-
-        return TextOutput(text=str(response))
-
-    asyncio.run(run_agent_provider(server))
-
-
-if __name__ == "__main__":
-    main()
+    return TextOutput(text=str(response))
