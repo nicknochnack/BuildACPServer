0119aa4cd6bc3ad8ec11223ac5f422e12cf3d0b9
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-04-08 13:28:46 +1000

    fix context

--- src/acpserver/server.py
+++ src/acpserver/server.py
@@ -1,22 +1,22 @@
 from beeai_sdk.providers.agent import Server
-from beeai_sdk.schemas.text import TextOutput, TextInput
+from beeai_sdk.schemas.text import TextOutput, TextInput, Con
 from smolagents import CodeAgent, DuckDuckGoSearchTool, LiteLLMModel, VisitWebpageTool
 
 server = Server("randoagentserver")
 
+
 @server.agent()
-async def run_crewai_agent(input: TextInput, ctx: Context) -> TextOutput:
-    
+async def run_crewai_agent(input: TextInput) -> TextOutput:
+
     model = LiteLLMModel(
-            model_id="ollama_chat/qwen2.5:14b",
-            api_base="http://localhost:11434",  
-            api_key="your-api-key",
-            num_ctx=8192) 
+        model_id="ollama_chat/qwen2.5:14b",
+        api_base="http://localhost:11434",
+        api_key="your-api-key",
+        num_ctx=8192,
+    )
 
     agent = CodeAgent(tools=[DuckDuckGoSearchTool(), VisitWebpageTool()], model=model)
 
-    response = agent.run(
-        input.text
-    )
+    response = agent.run(input.text)
 
     return TextOutput(text=str(response))
